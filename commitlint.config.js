aa13340fe03430ff29ea5aca6c076762ccf19819
Dmytro Hudym <d.hudym@temabit.com>
2026-01-28 17:55:23 +0200

    fix(ci): add release scope to commitlint for semantic-release

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [
-      2,
-      'always',
-      ['vault', 'repo', 'ci', 'docs', 'deps']
-    ]
-  }
+    'scope-enum': [2, 'always', ['vault', 'repo', 'ci', 'docs', 'deps', 'release']],
+  },
 };
